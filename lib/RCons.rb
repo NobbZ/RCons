8955b3904a0613be5eb20e496e9df00b24bc13b1
Norbert Melzer <timmelzer@gmail.com>
2013-09-17 08:50:57 +0200

    Creating and displaying the depGraph
    Usage:
    
    ```bash
    $ rcons graph			# Creates the depgraph
    $ rcons graph -o	# Creates and opens the depgraph for viewing
    ```

--- lib/RCons.rb
+++ lib/RCons.rb
@@ -1,27 +1,62 @@
-require "RCons/version"
+require 'RCons/version'
 
 module RCons
-  
+
   # Checks if the current ruby version is compatible with RCons. Even if the 
   # gemspec specifies at least ruby 1.9.3 I do this test just to be sure not to
   # get any exceptions thrown in mid of compiling process!
   def checkRubyVersion
-    $logger.debug "Checking for ruby version"
+    $logger.debug 'Checking for ruby version'
     $logger.debug "ruby version found: #{Gem.ruby_version}; needed: >= 1.9.3"
-    if Gem.ruby_version() < Gem::Version.create("1.9.3")
+    if Gem.ruby_version() < Gem::Version.create('1.9.3')
       $logger.fatal "Ruby Version NOT OK, required ruby version is >= 1.9.3!\n"
       exit 1
     end
   end
-  
+
   # Evaluates CLI arguments
   #
   # @param [Array<String>] args The arguments from the commandline
   def clistart(args)
+    $logger.debug 'Invoked with params:'
+    $logger.debug args
     if args.is_a? Array
+      case args[0]
+      when 'graph'
+        $logger.info 'Recognized command \'graph\''
+        drawGraph(args.include?("-o") | args.include?("--open") | args
+        .include?("--view"))
+      else
+        $logger.info 'No command found in arguments, doing nothing'
+      end
+      $logger.info "Finished task '#{args[0]}'"
       true
     else
-      throw "Something went terribly wrong with CLI-arguments"
+      throw 'Something went terribly wrong with CLI-arguments'
+    end
+  end
+
+  # Draws a dependency-graph of the files and targets
+  #
+  # @param [Boolean] view Defines if the graph should be opened for viewing
+  #   after generation
+  #   <dl>
+  #     <di>`true`</di><dd>tries to open the graph in external viewer</dd>
+  #     <di>`false`</di><dd>just quit without viewing the graph</dd>
+  #   <dl>
+  def drawGraph(view = false)
+    $logger.info "Creating depGraph.dot"
+    File.open('depGraph.dot', 'wt') do |dotFile|
+      dotFile.print "digraph G {\n"
+      dotFile.print "  all [label=\"all\"]\n"
+      dotFile.print $allTarget.to_dot
+      dotFile.print "}\n"
+    end
+    $logger.info 'Creating depGrpah.svg'
+    `dot -Tsvg -odepGraph.svg depGraph.dot`
+    if view
+      $logger.info 'Opening depGraph.svg'
+      `xdg-open depGraph.svg`
     end
   end
 end
--- lib/RCons/DSL.rb
+++ lib/RCons/DSL.rb
@@ -17,28 +17,47 @@ module RCons
       #     real own target. It is used to organize other targets under a common
       #     Target.
       def initialize(name, type)
+        $logger.debug "Target.new #{name}, :#{type}"
+
         @name         = name
         @type         = type
         @dependencies = []
+        @parents      = []
+
+        case File.extname(@name)
+        when '.c'
+          $logger.info "Guessed type of #{@name} to ':source'"
+          @type = :source if @type == :guess
+          $logger.info "Creating ':intermediate' target '#{File.basename(@name, File.extname(@name))}.o'"
+          inter = Target.new("#{File.basename(@name, File.extname(@name))}.o", :intermediate)
+          self.parents << inter
+          inter.add_dependency self
+        end
       end
 
       # Adds an dependancy to the target
       #
-      # @param  [Target, String, Array<Target, String>]  dependancy  The Target
-      #   that this Target depends on (please remember, that even a source file 
+      # @param  [Target, String, Array<Target, String>]  dependency  The Target
+      #   that this Target depends on (please remember, that even a source file
       #   is considered a Target!).
-      def add_dependancy(dependancy)
-        if dependancy.is_a? Target
-          @dependencies << dependancy
-        elsif dependancy.is_a? String
-          @dependencies << Target.new(dependancy, :guess)
-        elsif dependancy.is_a? Array
-          dependancy.each do |d|
-            self.add_dependancy(d)
+      def add_dependency(dependency)
+        $logger.debug "Target '#{@name}'.add_dependency #{dependency}"
+        if dependency.is_a? Target
+          @dependencies << dependency
+        elsif dependency.is_a? String
+          @dependencies << Target.new(dependency, :guess)
+        elsif dependency.is_a? Array
+          dependency.each do |d|
+            self.add_dependency(d)
           end
         else
-          $logger.warn "WARNING: #{dependancy} is not a valid Target!"
+          $logger.warn "WARNING: #{dependency} is not a valid Target!"
         end
+        $logger.debug @dependencies
+      end
+
+      def parents
+        @parents
       end
 
       def dependencies
@@ -48,6 +67,19 @@ module RCons
       def to_s
         @name
       end
+
+      def to_dot
+        $logger.debug "Depgraphing #{@name}"
+        retval = "#{@name.downcase.gsub(/[^a-z0-9]/,
+                                        '')}\t[label=\"#{@name}\"]\n"
+        @dependencies.each do |target|
+          retval += target.to_dot
+          retval += "#{@name.downcase.gsub(/[^a-z0-9]/, '')} -> "
+          retval += "#{target.to_s.downcase.gsub(/[^a-z0-9]/, '')}\n"
+        end
+
+        retval
+      end
     end
 
     # Creates an executable with the name target by compiling the sources.
@@ -56,21 +88,23 @@ module RCons
     #   fileextensions!
     # @param [String, Array<String>]  sources An array of strings which contains
     #   the necessary sourcefiles, relative to the RCons-Script
-    # 
+    #
     # @example
     #   executable "example", ["hello.c", "world.c"]
     # This will take `hello.c` and `world.c` and compile each of
     # them, afterwards the resulting `hello.o` and `world.o` will
     # be linked together into `example`
     def executable(target, sources)
+      $logger.info "Found target '#{target}' with following sources/dependencies:"
+      $logger.info sources
       target = Target.new target, :executable
-      $allTarget.add_dependancy target
+      $allTarget.add_dependency target
       if sources.is_a? String
         sources = [sources]
       end
-      sources.each_index do |i|
-        t = Target.new(sources[i], :guess)
-        target.add_dependancy t
+      sources.each do |s|
+        t = Target.new(s, :guess)
+        target.add_dependency t.parents
       end
     end
   end
