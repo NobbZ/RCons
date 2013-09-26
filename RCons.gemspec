2df4c1e16d0e559e52a78592ea28718ffec4def0
Norbert Melzer <timmelzer@gmail.com>
2013-09-26 15:19:19 +0200

    Beginning from scratch!

--- RCons.gemspec
+++ RCons.gemspec
@@ -20,13 +20,6 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = %w(lib)
 
-  spec.add_dependency 'logging'
-  spec.add_dependency 'ruby-graphviz'
-  spec.add_dependency 'thor'
-
   spec.add_development_dependency 'bundler', '~> 1.3'
   spec.add_development_dependency 'rake'
-  spec.add_development_dependency 'redcarpet'
-  spec.add_development_dependency 'rspec'
-  spec.add_development_dependency 'yard'
 end
--- lib/RCons.rb
+++ lib/RCons.rb
@@ -1,39 +1,4 @@
-require 'graphviz'
-
 require 'RCons/version'
 
 module RCons
-
-  # Checks if the current ruby version is compatible with RCons. Even if the 
-  # gemspec specifies at least ruby 1.9.3 I do this test just to be sure not to
-  # get any exceptions thrown in mid of compiling process!
-  def self.check_ruby_version
-    $logger.debug 'Checking for ruby version'
-    $logger.debug "ruby version found: #{Gem.ruby_version}; needed: >= 1.9.3"
-    if Gem.ruby_version() < Gem::Version.create('1.9.3')
-      $logger.fatal "Ruby Version NOT OK, required ruby version is >= 1.9.3!\n"
-      exit 1
-    end
-  end
-
-  # Draws a dependency-graph of the files and targets
-  #
-  # @param [Boolean] view Defines if the graph should be opened for viewing
-  #   after generation
-  #   <dl>
-  #     <di>`true`</di><dd>tries to open the graph in external viewer</dd>
-  #     <di>`false`</di><dd>just quit without viewing the graph</dd>
-  #   <dl>
-  def self.draw_graph(view = false)
-    $logger.info 'Creating depGraph in memory'
-    graph = GraphViz::new(:G, type: :digraph) do |g|
-      $all_target.to_dot g
-    end
-    $logger.info 'Saving depGrpah.svg'
-    graph.output(svg: 'depGraph.svg', nothugly: true)
-    if view
-      $logger.info 'Opening depGraph.svg'
-      `xdg-open depGraph.svg`
-    end
-  end
 end
